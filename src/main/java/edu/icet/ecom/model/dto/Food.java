3aa5b9b4e79be112e6f78ab69a7ba5e6958f9455
imash2006 <imashkottage24@gmail.com>
2025-11-02 11:50:48 +0530

    food model class, service class and the controller classes were created

---
+++ src/main/java/edu/icet/ecom/model/dto/Food.java
@@ -0,0 +1,15 @@
+package edu.icet.ecom.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Food {
+    private String name;
+    private String details;
+    private String quantity;
+    private Double price;
+}
